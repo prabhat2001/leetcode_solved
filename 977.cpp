a5e008fc537f600f34571a15c78fc954178188e0
Prabhat Dwivedi <prabhatdwivedi0208@gmail.com>
2022-06-14 08:28:44 +0530

    Squares of a Sorted Array

---
+++ 977.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> v;
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r){
+            if(nums[l]*nums[l]<nums[r]*nums[r]){
+            v.push_back(nums[r]*nums[r]);
+            r--;
+        }
+        else{
+            v.push_back(nums[l]*nums[l]);
+            l++;
+        }
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
