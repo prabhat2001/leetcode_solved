ffec7018bd6dc6d621950b46196cafc3bcb37f12
Prabhat Dwivedi <prabhatdwivedi0208@gmail.com>
2022-06-17 08:04:28 +0530

    Replace Elements with Greatest Element on Right Side

---
+++ 1299.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+       
+        for(int i=0;i<arr.size();i++){
+            int max=arr[i+1];
+            for(int j=i+1;j<arr.size();j++){
+                if(max<arr[j]){
+                    max=arr[j];
+                }
+                
+                arr[i]=max;
+                
+            }
+            
+         
+        }
+          arr[arr.size()-1]=-1;
+          return arr;
+    }
+};
