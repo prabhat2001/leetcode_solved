5a24bf71e32a0746ec74578b1e734b03a80f786c
Prabhat Dwivedi <prabhatdwivedi0208@gmail.com>
2022-06-18 11:19:59 +0530

    Contains Duplicates

---
+++ 217.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> a;
+        for(auto i:nums) a.insert(i);
+        if (nums.size() == a.size()) return false;
+        return true;
+    }
+};
