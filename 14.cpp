c6dabdc1fe9111e2031fd017084090d27979958b
Prabhat Dwivedi <prabhatdwivedi0208@gmail.com>
2022-07-17 09:04:27 +0530

    Longest Common Prefix

---
+++ 14.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        ios::sync_with_stdio(false); cin.tie(nullptr);
+        string pref = "";
+        int n = strs.size(), m = strs[0].size();
+
+        sort(strs.begin(), strs.end());
+        
+        for(int i = 0; strs[0][i] == strs[n-1][i] && i < m; i++){
+            pref += strs[0][i];
+        }
+        
+        return pref;
+    }
+};
